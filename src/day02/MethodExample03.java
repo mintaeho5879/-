9c1763bd25ad534034b3cee34c8b5dc70d38bf89
minbonjwa <minbonjwa@gmail.com>
2025-06-05 21:29:06 +0900

    first

--- src/day02/MethodExample03.java
+++ src/day02/MethodExample03.java
@@ -13,7 +13,7 @@ public class MethodExample03 {
     }
     public static int[] deepCopy(int[] arr) {
         int[] arr2 = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < arr.length; i++) {
            arr2[i] = arr[i];
         }
         return arr2;
